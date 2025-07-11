29b93769d79e6f79e7a54196a00274c9c1a5f2e7
ThomasGiardina <thomasgiardina0@gmail.com>
2025-07-11 09:09:40 -0300

    comentario

--- demo/src/main/java/com/uade/dam/demo/DemoApplication.java
+++ demo/src/main/java/com/uade/dam/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // ← Agregar esta anotación
+@EnableScheduling 
 public class DemoApplication {
 
     public static void main(String[] args) {
